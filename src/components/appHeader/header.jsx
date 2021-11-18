b50b4b79d427cf729794e1706ccaab20478eb033
aman <442844516@qq.com>
2021-11-18 18:17:53 +0800

    seconde

--- src/components/appHeader/header.jsx
+++ src/components/appHeader/header.jsx
@@ -1,12 +1,61 @@
 import './header.css'
+import { Popover, Toast } from 'antd-mobile'
+import { useState, useEffect } from 'react'
+
+
 
 const Header = () => {
+    const states = [
+        {
+            text: '空闲',
+            style: {
+                background: "rgb(59, 216, 119)"
+            },
+        },
+        {
+            text: '任务11111111111111111111111111111111',
+            style: {
+                background: "rgb(100, 116, 119)"
+            },
+        },
+        {
+            text: '任务3',
+            style: {
+                background: "rgb(100, 6,0 )"
+            },
+        }
+    ]
+    let [title, setTitle] = useState({
+        text: '空闲',
+        style: {
+            background: "rgb(59, 216, 119)"
+        },
+    })
+
+    const check = (node) => {
+        setTitle(node)
+        Toast.show(`已切换 ${node.text}`)
+    }
+
+
     return (
         <div id="header">
-            <div className="states flexSA">
-                <img src="./img/确认.png" alt="" />
-                空闲
-            </div>
+            <Popover.Menu
+                actions={states}
+                onAction={(node) => { check(node) }}
+                placement='bottomLeft'
+                trigger='click'
+            >
+                <div className="states" style={title.style}>
+                    <span className="statesImg flexC">
+                        <img src="./img/确认.png" alt="" />
+                    </span>
+                    <span className="stateText flexC">
+                        {title.text}
+                    </span>
+                </div>
+            </Popover.Menu>
+
             <div className="coordinate flexSA">
                 <img src="./img/坐标.png" alt="" />
                 x:12,221 y:23,12, yaw:21
